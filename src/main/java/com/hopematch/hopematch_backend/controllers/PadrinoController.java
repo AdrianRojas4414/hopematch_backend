9d24d1075d8ea51468d42f11ba8c4c7017c004df
adrian <adrian.rojas.o@gmail.com>
2025-03-13 00:48:39 -0400

    + Get hello world para pruebas

--- src/main/java/com/hopematch/hopematch_backend/controllers/PadrinoController.java
+++ src/main/java/com/hopematch/hopematch_backend/controllers/PadrinoController.java
@@ -3,6 +3,8 @@ package com.hopematch.hopematch_backend.controllers;
 import com.hopematch.hopematch_backend.models.Padrino;
 import com.hopematch.hopematch_backend.services.PadrinoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,5 +18,10 @@ public class PadrinoController {
     @PostMapping("/addPadrino")
     public Padrino postPadrino(@RequestBody Padrino padrino){
         return padrinoService.savePadrino(padrino);
+    }
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> helloWorld(){
+        return ResponseEntity.ok("Hello World! Esto es HOPEMATCH");
     }
 }
--- src/main/java/com/hopematch/hopematch_backend/repositories/PadrinoRepository.java
+++ src/main/java/com/hopematch/hopematch_backend/repositories/PadrinoRepository.java
@@ -4,4 +4,5 @@ import com.hopematch.hopematch_backend.models.Padrino;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PadrinoRepository extends JpaRepository<Padrino,Integer> {
+
 }
