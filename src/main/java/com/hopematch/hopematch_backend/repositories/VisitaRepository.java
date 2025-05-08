9ffb3463d99a2719b82def8602e9f87b497182db
Reynaldo <reynaldo.ruiz@ucb.edu.bo>
2025-05-07 23:00:03 -0400

    implementacion de aceptar/denegar visitas registradas

--- src/main/java/com/hopematch/hopematch_backend/repositories/VisitaRepository.java
+++ src/main/java/com/hopematch/hopematch_backend/repositories/VisitaRepository.java
@@ -3,7 +3,14 @@ package com.hopematch.hopematch_backend.repositories;
 import com.hopematch.hopematch_backend.models.Visita;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface VisitaRepository extends JpaRepository<Visita, Integer> {
+    List<Visita> findByEncargadoIdAndEsperandoRespuesta(int encargadoId, boolean esperandoRespuesta);
+
+    List<Visita> findByPadrinoId(int padrinoId);
+
+    Optional<Visita> findByIdAndEncargadoId(int id, int encargadoId);
 }
