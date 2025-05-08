ae8c71aa3461ad3c48eb0671b76cdfe8acd5f5d1
Joshx567 <105055532+Joshx567@users.noreply.github.com>
2025-05-08 14:32:05 -0400

    Hm4 05 enviar/recibirmensajesdepadrinos (#28)
    * feat: se agrego la rama HM4-02: Enviar/Recibir mensajes entre padrinos y encargados con una funcionalidad basica
    
    * feat: se agrego la rama HM4-05: Enviar/Recibir mensajes entre padrinos y encargados con una funcionalidad actualizada
    
    ---------
    
    Co-authored-by: AdrianRojas4414 <142945475+AdrianRojas4414@users.noreply.github.com>

--- src/main/java/com/hopematch/hopematch_backend/models/Mensaje.java
+++ src/main/java/com/hopematch/hopematch_backend/models/Mensaje.java
@@ -17,27 +17,40 @@ public class Mensaje {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @Column(name = "id_padrino")
+    @Column(name = "id_padrino", nullable = false)
     private int idPadrino;
 
-    @Column(name = "id_encargado")
+    @Column(name = "id_encargado", nullable = false)
     private int idEncargado;
 
-    @Column(name = "destinatario", length = 50)
+    @Column(name = "remitente", length = 50, nullable = false)
+    private String remitente;
+
+    @Column(name = "destinatario", length = 50, nullable = false)
     private String destinatario;
 
-    @Column(name = "mensaje", columnDefinition = "TEXT")
+    @Column(name = "mensaje", columnDefinition = "TEXT", nullable = false)
     private String mensaje;
 
-    @Column(name = "fecha", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    @Column(name = "mensaje_respuesta", columnDefinition = "TEXT")
+    private String mensajeRespuesta;
+
+    @Column(name = "fecha", nullable = false)
     private LocalDateTime fecha;
 
-    // Opcional: puedes generar un constructor sin `fecha` si prefieres que se asigne automáticamente
-    public Mensaje(int idPadrino, int idEncargado, String destinatario, String mensaje) {
+    @Column(name = "leido", nullable = false)
+    private boolean leido;
+
+    // Constructor sin fecha y leido predeterminado a false
+    public Mensaje(int idPadrino, int idEncargado, String remitente, String destinatario, String mensaje, String mensaje_respuesta) {
         this.idPadrino = idPadrino;
         this.idEncargado = idEncargado;
+        this.remitente = remitente;
         this.destinatario = destinatario;
         this.mensaje = mensaje;
+        this.mensajeRespuesta = mensaje_respuesta;
         this.fecha = LocalDateTime.now();
+        this.leido = false;  // Por defecto el mensaje no ha sido leído
     }
 }
+
